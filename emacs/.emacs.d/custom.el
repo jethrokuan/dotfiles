b1c62bab4a5364b2e9a2e77b68f522e32af47619
Jethro Kuan <jethrokuan95@gmail.com>
2016-06-15 17:23:44 +0800

    Update emacs config

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -9,7 +9,8 @@
  '(anzu-replace-to-string-separator " => ")
  '(anzu-search-threshold 1000)
  '(paradox-automatically-star t)
- '(paradox-github-token "e0f4a9cba91185c65b8170f45ee7941c61b88552"))
+ '(paradox-github-token "e0f4a9cba91185c65b8170f45ee7941c61b88552")
+ '(safe-local-variable-values (quote ((firestarter-type . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -28,5 +29,4 @@
  '(org-level-1 ((t (:background "gainsboro" :foreground "#080808" :height 1.5))))
  '(org-level-2 ((t (:slant italic :height 1.2))))
  '(org-meta-line ((t (:inherit font-lock-comment-face :foreground "#3C3C3C" :height 1.0))))
- '(org-todo ((t (:foreground "dim gray" :weight bold))))
- )
+ '(org-todo ((t (:foreground "dim gray" :weight bold)))))
