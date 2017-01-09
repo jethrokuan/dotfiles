9e64778e54ecd58aee0e6a2d17cbe415b62f1b49
Jethro Kuan <jethrokuan95@gmail.com>
2017-01-10 03:19:58 +0800

    add eclim help pointer

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -456,7 +456,10 @@
 
 (use-package eclim
   :config 
-  (global-eclim-mode))
+  (global-eclim-mode)
+  (setq help-at-pt-display-when-idle t)
+  (setq help-at-pt-timer-delay 0.1)
+  (help-at-pt-set-timer))
 
 (use-package company-emacs-eclim
   :config
