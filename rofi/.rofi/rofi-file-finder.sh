ab1d0f6da74d0a166421f10380c638694d8a0c03
Jethro Kuan <jethrokuan95@gmail.com>
2017-01-13 10:26:28 +0800

    Add rofi scripts

---
+++ rofi/.rofi/rofi-file-finder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+usage()
+{
+    cat << EOF
+usage: $0 options
+
+Index and search files using dmenu
+
+OPTIONS:
+   -h      Show this message
+   -o      Store index [default: /tmp/fmenu_index]
+   -i      Search folders [default: $HOME]
+   -x      Exclude files [default: ".*\/..*" (i.e. dot-files)]
+   -d      dmenu parameters [default: -i -l 20]
+   -f      force reloading index [default: false]
+   -t      time [default: 5min]
+   -u      Just update the index
+EOF
+}
+
+if [ -z $@ ]
+then
+    locate /home/jethro/
+else
+    xdg-open $@ > /dev/null &
+fi
