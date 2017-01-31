f548d8ed6bbd28164e18e19cf44a0665e9fed4b8
Jethro Kuan <jethrokuan95@gmail.com>
2017-01-31 19:43:23 +0800

    Add LaTeX support

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -11,9 +11,12 @@
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "321abea659ad5639f1bb3ed5215ec498fe819dcd09f5af7b5797a0f6177972d1" "70ae149c82ca1a7c7968584bfe8c571349b51463cf1322f4b126c36b57d4606f" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(epresent-text-scale 300)
+ '(org-agenda-files
+   (quote
+    ("~/.org/gtd/daily.org" "~/.org/gtd/emacs.org" "~/.org/gtd/inbox.org" "~/.org/gtd/money.org" "~/.org/gtd/nushackers.org" "~/.org/gtd/personal.org" "~/.org/gtd/school.org" "~/.org/gtd/weekly.org")))
  '(package-selected-packages
    (quote
-    (github-theme anzu anzu-mode company-emacs-eclim gradle-mode eclim org-plus-contrib org-with-contrib notmuch mu4e ample-theme apropospriate-theme iedit atomic-chrome deft skewer-mode slime visual-regexp visual-regexp-steroids js-comint epresent smartparens jade org-download validate magithub ag bury-successful-compilation darkroom artbollocks-mode focus paradox firestarter nameless which-key pivotal-tracker keyfreq esup counsel-projectile projectile magit org-pomodoro ox-reveal htmlize git-gutter-fringe+ volatile-highlights golden-ratio beacon nyan-mode smart-mode-line web-mode vue-mode use-package scss-mode rust-mode rainbow-mode page-break-lines neotree move-text markdown-mode key-chord json-mode js2-refactor go-mode flycheck-pos-tip flycheck-clojure flx fish-mode expand-region exec-path-from-shell emmet-mode elpy dumb-jump doom-themes crux counsel company-tern company-quickhelp clj-refactor avy aggressive-indent)))
+    (company-auctex auctex-latexmk auctex latex-preview-pane github-theme anzu anzu-mode company-emacs-eclim gradle-mode eclim org-plus-contrib org-with-contrib notmuch mu4e ample-theme apropospriate-theme iedit atomic-chrome deft skewer-mode slime visual-regexp visual-regexp-steroids js-comint epresent smartparens jade org-download validate magithub ag bury-successful-compilation darkroom artbollocks-mode focus paradox firestarter nameless which-key pivotal-tracker keyfreq esup counsel-projectile projectile magit org-pomodoro ox-reveal htmlize git-gutter-fringe+ volatile-highlights golden-ratio beacon nyan-mode smart-mode-line web-mode vue-mode use-package scss-mode rust-mode rainbow-mode page-break-lines neotree move-text markdown-mode key-chord json-mode js2-refactor go-mode flycheck-pos-tip flycheck-clojure flx fish-mode expand-region exec-path-from-shell emmet-mode elpy dumb-jump doom-themes crux counsel company-tern company-quickhelp clj-refactor avy aggressive-indent)))
  '(paradox-github-token t)
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "smtp.gmail.com")
