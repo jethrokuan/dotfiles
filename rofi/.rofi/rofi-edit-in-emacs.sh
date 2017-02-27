d502151599424ccd3b3fe90aa20f141f272daca5
Jethro Kuan <jethrokuan95@gmail.com>
2017-02-27 10:31:26 +0800

    Update rofi scripts

---
+++ rofi/.rofi/rofi-edit-in-emacs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+    rg $HOME --files
+else
+    emacsclient -n -e "(if (> (length (frame-list)) 1) 't)" | grep t > /dev/null
+    if [ "$?" = "1" ]; then
+        emacsclient -c -n -a "" "$@" > /dev/null &
+    else
+        emacsclient -n -a "" "$@" > /dev/null & 
+    fi
+    exit 0
+fi
