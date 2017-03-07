a86ed1ab6d45eb6b671d1b81966ea1263ab29e03
Jethro Kuan <jethrokuan95@gmail.com>
2017-03-07 14:46:33 +0800

    fix rofi-folder showing duplicate names

--- rofi/.rofi/rofi-folder.sh
+++ rofi/.rofi/rofi-folder.sh
@@ -2,7 +2,7 @@
 
 if [ -z $@ ]
 then
-    rg --files --null 2> /dev/null | xargs -0 dirname | uniq
+    rg --sort-files --files --null 2> /dev/null | xargs -0 dirname | uniq
 else 
     xdg-open "$@" > /dev/null &
 fi
