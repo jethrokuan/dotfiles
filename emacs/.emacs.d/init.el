464d2783bf3adac27c39da1d39366428085487a9
Jethro Kuan <jethrokuan95@gmail.com>
2017-03-13 09:29:40 +0800

    use xdg-open instead

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -199,7 +199,34 @@
           ("M-D" . crux-duplicate-and-comment-current-line-or-region)
           ("s-o" . crux-smart-open-line-above)))
 
-(bind-key* "<f8>" 'browse-url-of-buffer)
+(defun jethro/open-in-external-app ()
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference."
+  (interactive)
+  (let* (
+         (-file-list
+          (if (string-equal major-mode "dired-mode")
+              (dired-get-marked-files)
+            (list (buffer-file-name))))
+         (-do-it-p (if (<= (length -file-list) 5)
+                       t
+                     (y-or-n-p "Open more than 5 files? "))))
+    (when -do-it-p
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc
+         (lambda (-fpath)
+           (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" -fpath t t))) -file-list))
+       ((string-equal system-type "darwin")
+        (mapc
+         (lambda (-fpath)
+           (shell-command
+            (concat "open " (shell-quote-argument -fpath))))  -file-list))
+       ((string-equal system-type "gnu/linux")
+        (mapc
+         (lambda (-fpath) (let ((process-connection-type nil))
+                            (start-process "" nil "xdg-open" -fpath))) -file-list))))))
+(bind-key* "<f8>" 'jethro/open-in-external-app)
 
 (use-package anzu
   :diminish anzu-mode 
