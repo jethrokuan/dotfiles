f525550ec0508d204b725483ceb72d42cfd49dc9
Jethro Kuan <jethrokuan95@gmail.com>
2017-05-08 22:10:30 +0800

    Re-enable double quote pair

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -311,8 +311,7 @@ The app is chosen from your OS's preference."
 
   ;; Org-mode config
   (sp-with-modes 'org-mode
-    (sp-local-pair "'" nil :actions :rem)
-    (sp-local-pair "\"" nil :actions :rem)
+    (sp-local-pair "'" nil :actions :rem)        
     (sp-local-pair "*" "*" :actions '(insert wrap) :unless '(sp-point-after-word-p sp-point-at-bol-p) :wrap "C-*" :skip-match 'sp--org-skip-asterisk)
     (sp-local-pair "_" "_" :unless '(sp-point-after-word-p) :wrap "C-_")
     (sp-local-pair "/" "/" :unless '(sp-point-after-word-p) :post-handlers '(("[d1]" "SPC")))
