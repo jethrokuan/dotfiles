1895619e2d1d37d77c1b27cdd527557693754a2c
Jethro Kuan <jethrokuan95@gmail.com>
2017-05-22 11:38:07 +0800

    Update README
    - and track fisher.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
 for file in ~/.config/fish/conf.d/*.fish
     source $file
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/jethrokuan/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/jethrokuan/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/jethrokuan/Downloads/google-cloud-sdk/path.fish.inc'; end; end
