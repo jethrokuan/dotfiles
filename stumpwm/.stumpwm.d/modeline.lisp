a6350dcf1fed6351e90de4b9cdf913d30b4f0c0d
Jethro Kuan <jethrokuan95@gmail.com>
2017-05-22 23:38:16 +0800

    use zenburn theme for modeline too

--- stumpwm/.stumpwm.d/modeline.lisp
+++ stumpwm/.stumpwm.d/modeline.lisp
@@ -1,27 +1,19 @@
 (load-module "battery-portable")
 (load-module "wifi")
-;; "[^B%n^b] %W"
-;; "%n%s%t"
-;; "%m%n%s%50t"
-;; "%a %b %e %k:%M:%S"
 
 (setf *bar-med-color* "^B^3")
 (setf *bar-hi-color* "^B^3")
 (setf *bar-crit-color* "^B^1")
 
 (setf *colors*
-      '("black"
-        "#FFDC00"
-        "#01FF70"
-        "#DDDDDD"
-        "#DDDDDD"
-        "#DDDDDD"
-        "#DDDDDD"
-        "#DDDDDD"
-        "#DDDDDD"
-        "#DDDDDD"
-        "#DDDDDD"
-        "#DDDDDD"
+      `(,zenburn-fg
+        ,zenburn-orange
+        ,zenburn-green 
+        ,zenburn-cyan
+        ,zenburn-blue
+        ,zenburn-red
+        ,zenburn-magenta
+        ,zenburn-yellow
         ))
 
 (update-color-map (current-screen))
@@ -29,30 +21,24 @@
 (setf *group-format* " %t ")
 ;;(setf *window-format* "%m%50t ")
 (setf *window-format* "%m%n%s%10t ")
-(setf *mode-line-timeout* 2)
+(setf *mode-line-timeout* 1)
 
-(setf *time-modeline-string* "^3 %e %b^n^B %l:%M ^b")
+(setf *time-modeline-string* "^B^3 %e %b %T ^n")
 
 (defun get-date-modeline ()
   (run-shell-command
    (format nil "date +\"~A\""
            *time-modeline-string*) t))
 
-(defun get-layout-modeline ()
-  (if (= 0 (get-current-layout *display*))
-      "^2 en ^n"))
-
 (setf *screen-mode-line-format*
-      (list "^B^3 %g ^n^b %W ^> "
-            '(:eval (get-layout-modeline))
-            "  "
-            "^B^3^n^b%B "
+      (list "^B^3 %g ^n^b %W ^> " 
+            "^B^3^n^b%B  "
             '(:eval (get-date-modeline))
             ))
 
-(setf *mode-line-border-width* 0)
-(setf *mode-line-background-color* "#000809")
-(setf *mode-line-foreground-color* "#01FF70")
+(setf *mode-line-border-width* 1)
+(setf *mode-line-background-color* zenburn-bg)
+(setf *mode-line-foreground-color* zenburn-fg)
 
 (if (not (head-mode-line (current-head)))
     (toggle-mode-line (current-screen) (current-head)))
--- stumpwm/.stumpwm.d/theme.lisp
+++ stumpwm/.stumpwm.d/theme.lisp
@@ -1,8 +1,42 @@
-;; Sarcasm personnal theme for StumpWM
-;; usage (with user-module):
-;; (load-user-module "sarcasm-theme")
-
 (in-package :stumpwm)
+
+(setq zenburn-fg+1      "#FFFFEF")
+(setq zenburn-fg        "#DCDCCC")
+(setq zenburn-fg-1      "#656555")
+(setq zenburn-bg-2      "#000000")
+(setq zenburn-bg-1      "#2B2B2B")
+(setq zenburn-bg-05     "#383838")
+(setq zenburn-bg        "#3F3F3F")
+(setq zenburn-bg+05     "#494949")
+(setq zenburn-bg+1      "#4F4F4F")
+(setq zenburn-bg+2      "#5F5F5F")
+(setq zenburn-bg+3      "#6F6F6F")
+(setq zenburn-red+1     "#DCA3A3")
+(setq zenburn-red       "#CC9393")
+(setq zenburn-red-1     "#BC8383")
+(setq zenburn-red-2     "#AC7373")
+(setq zenburn-red-3     "#9C6363")
+(setq zenburn-red-4     "#8C5353")
+(setq zenburn-orange    "#DFAF8F")
+(setq zenburn-yellow    "#F0DFAF")
+(setq zenburn-yellow-1  "#E0CF9F")
+(setq zenburn-yellow-2  "#D0BF8F")
+(setq zenburn-green-1   "#5F7F5F")
+(setq zenburn-green     "#7F9F7F")
+(setq zenburn-green+1   "#8FB28F")
+(setq zenburn-green+2   "#9FC59F")
+(setq zenburn-green+3   "#AFD8AF")
+(setq zenburn-green+4   "#BFEBBF")
+(setq zenburn-cyan      "#93E0E3")
+(setq zenburn-blue+1    "#94BFF3")
+(setq zenburn-blue      "#8CD0D3")
+(setq zenburn-blue-1    "#7CB8BB")
+(setq zenburn-blue-2    "#6CA0A3")
+(setq zenburn-blue-3    "#5C888B")
+(setq zenburn-blue-4    "#4C7073")
+(setq zenburn-blue-5    "#366060")
+(setq zenburn-magenta   "#DC8CC3")
+
 
 (defvar *wallpaper-command* "nitrogen --restore"
   "Wallpaper command.")
@@ -18,21 +52,21 @@
 (setq clx-truetype:*font-dirs* '("/run/current-system/sw/share/X11-fonts/"))
 (clx-truetype:cache-fonts)
 
-(set-font (make-instance 'xft:font :family "Droid Sans Mono" :subfamily
-                         "Regular" :size 10))
+(set-font (make-instance 'xft:font :family "Droid Sans" :subfamily
+                         "Regular" :size 11))
 
-(set-focus-color "#01FF70")
-(set-unfocus-color "#3D9970")
+(set-focus-color zenburn-green)
+(set-unfocus-color zenburn-green-1)
 
 
-(setf *maxsize-border-width* 2)
-(setf *transient-border-width* 2)
-(setf *normal-border-width* 2)
+(setf *maxsize-border-width* 1)
+(setf *transient-border-width* 1)
+(setf *normal-border-width* 1)
 
-(set-fg-color "#EEEEEE")
-(set-bg-color "#3D9970")
-(set-msg-border-width 0)
-(set-border-color "#000000")
+(set-fg-color zenburn-fg)
+(set-bg-color zenburn-bg+1)
+(set-msg-border-width 1)
+(set-border-color zenburn-orange)
 
 (setf *message-window-padding* 50)
 (setf *message-window-gravity* :top
