c3466d3f59640b15c28b0486ae07e8d6cb2856c7
Jethro Kuan <jethrokuan95@gmail.com>
2017-05-28 21:24:03 +0800

    Update dropbox script

--- scripts/.scripts/dropbox.sh
+++ scripts/.scripts/dropbox.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
+stat=$(dropbox status)
 
-if dropbox status | grep -q "Up to date";
-then
-    word=DONE
-else
-    if dropbox status | grep -q Downloading;
-    then
-        word=DL
-    elif dropbox status | grep -q Uploading;
-    then
-        word=UL
-    else
-        word=ERR
-    fi
+if   echo $stat | grep -q "Up to date"; then word=DONE
+elif echo $stat | grep -q "Downloading"; then word=DL
+elif echo $stat | grep -q "Connecting"; then word=CONN
+elif echo $stat | grep -q "Uploading"; then word=UL
+else word=ERR
 fi
 
 echo -e "DB: $word";
