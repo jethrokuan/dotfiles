7d19ab8565396d09389ff6f8fdc1ea92d0835b5f
Jethro Kuan <jethrokuan95@gmail.com>
2017-06-05 20:26:51 +0800

    Add Stumpwm

--- stumpwm/.startup.sh
+++ stumpwm/.startup.sh
@@ -2,6 +2,8 @@ if ! test -z ~/.screenlayout/desktop.sh; then
     sh ~/.screenlayout/desktop.sh 
 fi
 
+feh --bg-scale $HOME/Pictures/wallpaper.png &
+
 compton --config $XDG_CONFIG_HOME/compton/compton.conf &
 
 dunst &
