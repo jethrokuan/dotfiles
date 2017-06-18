25750a50d2bae2bf7488cccb00d29041e8b58fdf
Jethro Kuan <jethrokuan95@gmail.com>
2017-06-18 16:47:41 +0800

    Add winner-mode to stumpwm

--- stumpwm/.stumpwm.d/windows.lisp
+++ stumpwm/.stumpwm.d/windows.lisp
@@ -17,3 +17,18 @@
 
 (define-frame-preference "chat"
     (0 t   t :class "Franz"))
+
+(load-module "winner-mode")
+
+(defvar *winner-map* nil
+  "keymap for winner-mode")
+
+(defkeys *winner-map*
+    ("h" "winner-undo")
+  ("s" "winner-redo"))
+
+(add-hook *post-command-hook* (lambda (command)
+                                (when (member command winner-mode:*default-commands*)
+                                  (winner-mode:dump-group-to-file))))
+
+(define-key *top-map* (kbd "s-w") *winner-map*)
