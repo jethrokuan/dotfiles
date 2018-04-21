043c212cc62241d878b37d6c22060d3187a6fa3f
Jethro Kuan <jethrokuan95@gmail.com>
2018-04-21 22:04:32 +0800

    launch xinit on start

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,9 @@ for file in ~/.config/fish/conf.d/*.fish
     source $file
 end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/jethrokuan/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/jethrokuan/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/jethrokuan/Downloads/google-cloud-sdk/path.fish.inc'; end; end
+# Start X at login
+if status --is-login
+    if test -z "$DISPLAY" -a (fgconsole) = 1
+        exec xinit -- -keeptty
+    end
+end
